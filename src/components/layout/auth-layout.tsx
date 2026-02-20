81fc98cd994a391f3348168ae6a8ea06f24be168
Chae-G-0 <corkdud@naver.com>
2026-02-20 23:27:49 +0900

    Feat: auth

--- src/components/layout/auth-layout.tsx
+++ src/components/layout/auth-layout.tsx
@@ -1,6 +1,10 @@
-import { Outlet } from "react-router";
+import { useSession } from "@/store/session";
+import { Navigate, Outlet } from "react-router";
 
 export default function AuthLayout() {
+  const session = useSession();
+  if (session) return <Navigate to={"/"} replace={true} />;
+
   return (
     <div className="flex min-h-screen flex-col items-center">
       <div className="m-auto w-[360px]">
---
+++ src/components/layout/global-loader.tsx
@@ -0,0 +1,7 @@
+export default function GlobalLoader() {
+  return (
+    <div className="bg-muted flex h-[100vh] w-[100vw] flex-col items-center justify-center">
+      로딩중
+    </div>
+  );
+}
